4eba114d72dd2bac02fb80abcdae56d0c2cfafef
ABDUR RAHMAN Z <104237399+Rahmxn@users.noreply.github.com>
2024-03-11 08:37:19 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/lifecyclemethods/MainActivity.java
+++ app/src/main/java/com/example/lifecyclemethods/MainActivity.java
@@ -1,14 +1,43 @@
-package com.example.lifecyclemethods;
+package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
-import android.os.Bundle;
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
 
-public class MainActivity extends AppCompatActivity {
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
     }
 }
